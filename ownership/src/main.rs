cd94d882f097ef3567d9ad9b84c325d7ba7ac1ca
Dan Prudhomme <dannyprud@yahoo.com>
2023-06-20 10:57:29 -0500

    Complete chapter 4

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -21,9 +21,32 @@ fn main() {
 
 
     r2.push_str(" car#");
-    println!("{:?}", string_len(s4));
-    
 
+    let r3 = String::from("New string on the block");
+
+    let new = &r3[..3];
+    let string = &r3[4..=9];
+
+    let r4 = String::from("testing second Thrid fourth fifth");
+
+    println!("{new} {string}");
+
+    println!("{}", first_word(&String::from("testing 1 2 3")));
+    println!("{}", word_selector(&r4, 0));
+    println!("{}", word_selector(&r4, 1));
+    println!("{}", word_selector(&r4, 2));
+    println!("{}", word_selector(&r4, 3));
+
+    println!("{:?}", string_len(s4));
+
+    let r3 = string_len(String::from("testing"));
+    println!("{:?}", {r3});
+
+    let a = [1, 2, 3, 4, 5];
+
+    let b = &a[2..4];
+
+    assert_eq!(b, &a[2..4]);
 
 }
 
@@ -74,4 +97,39 @@ fn reference_string_len(string: &String) -> usize {
 fn change_ref_string(string: &mut String) {
     string.push_str(" dog!");
     println!("{}", string);
+}
+
+fn first_word(s: &str) -> &str { //&str is a string slice type
+    let bytes = s.as_bytes();  //Converts a string into an array of bytes
+
+    for (i, &item) in bytes.iter().enumerate() {  //iter returns each element in the collection.  enumerate() returns each element as part of a tuple instead.   //The first element of the tuple rturned from enumerate is the index, the second is a reference to the element
+        if item == b' ' {  //b' ' a byte literal               
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn word_selector(s: &str, count: usize) -> &str {
+    let bytes = s.as_bytes();
+
+    let mut spaces = 0;
+    let mut prev_space = 0;
+
+    for (i, &items) in bytes.iter().enumerate() {
+        if items == b' ' {
+            spaces += 1;
+
+            if spaces == count {
+                prev_space = i;
+            }
+
+            if spaces > count {
+                return &s[if count == 0 {prev_space} else {prev_space + 1}..i];
+            }
+        } 
+    }
+
+    &s[..]
 }
