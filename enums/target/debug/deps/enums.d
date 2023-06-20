ffc24308b0f3ca7d4df00b7bf7989dd7a4893175
Dan Prudhomme <dannyprud@yahoo.com>
2023-06-20 16:11:12 -0500

    Start Chapter 6 (enums)

---
+++ enums/target/debug/deps/enums.d
@@ -0,0 +1,5 @@
+C:\Users\Courtland\Desktop\Dan Projects\rust\rust-book\enums\target\debug\deps\enums.exe: src\main.rs
+
+C:\Users\Courtland\Desktop\Dan Projects\rust\rust-book\enums\target\debug\deps\enums.d: src\main.rs
+
+src\main.rs:
---
+++ enums/target/debug/enums.d
@@ -0,0 +1,1 @@
+C:\Users\Courtland\Desktop\Dan\ Projects\rust\rust-book\enums\target\debug\enums.exe: C:\Users\Courtland\Desktop\Dan\ Projects\rust\rust-book\enums\src\main.rs
