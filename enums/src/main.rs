17b4c5c1b9d65b45742e15c6ea069a6553d56310
Dan Prudhomme <dannyprud@yahoo.com>
2023-06-21 11:18:57 -0500

    Complete section 6.2

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -39,6 +39,37 @@ impl Message {
     }
 }
 
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Arkansas,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quater(UsState),
+}
+
+impl Coin {
+    fn value_in_cents(&self) -> u8 {
+        match self {
+            Coin::Penny => {
+                println!("How LUCKY!?");
+                1
+            },
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quater(state) => {
+                println!("State quater from {:?}", state);
+                25
+            },
+        }
+    }
+}
+
 fn main() {
     let home = IpAddrKind::V4(Ipv4Addr {
         address: (127, 0, 0, 1),
@@ -64,4 +95,53 @@ fn main() {
     println!("{:#?}", some_number);
     println!("{:#?}", some_char);
     println!("{:#?}", absent_number);
+
+    let penny = Coin::Penny;
+    println!("{}", penny.value_in_cents());
+
+    let quater = Coin::Quater(UsState::Alaska);
+    println!("{}", quater.value_in_cents());
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("{:#?}", six);
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => { //other here can be named anything and I guess any variable that isn't defined already.  Will cover any arm that isn't specified.
+            println!("{:?}", other);
+            move_player(other)
+        }, 
+    }
+
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => { //Use this if you are just going to throw away the value and don't plan to use it at all in the arm.
+            reroll()
+        }, 
+    }
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (), //Do nothing, I guess the same as just saying return?
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        Some(i) => Some(i + 1),
+        None => None,
+    }
+}
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
+fn reroll() {
+    println!("REROLLING!");
 }
