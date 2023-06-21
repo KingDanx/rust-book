772d252145ef7a6a7e81ddd691f233f69b15489c
Dan Prudhomme <dannyprud@yahoo.com>
2023-06-21 10:05:54 -0500

    complte section 6.1

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,17 +1,67 @@
 #[derive(Debug)]
+struct Ipv4Addr {
+    address: (u8, u8, u8, u8),
+    protocol: String,
+}
+#[derive(Debug)]
+struct Ipv6Addr {
+    address: String,
+    protocol: String,
+}
+
+#[derive(Debug)]
 enum IpAddrKind {
-    V4,
-    V6
+    V4(Ipv4Addr),
+    V6(Ipv6Addr),
+}
+
+impl IpAddrKind {
+    fn log_protocol(&self) {
+        println!("{:#?}", self)
+    }
+}
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move {
+        x: i32,
+        y: i32,
+    },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        println!("Called");
+        println!("{:?}", self);
+    }
 }
 
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+    let home = IpAddrKind::V4(Ipv4Addr {
+        address: (127, 0, 0, 1),
+        protocol: String::from("Ipv4"),
+    });
 
-    route(four);
-    route(six);
-}
+    let loopback = IpAddrKind::V6(Ipv6Addr {
+        address: String::from("::1"),
+        protocol: String::from("Ipv6"),
+    });
 
-fn route(ip_kind: IpAddrKind) {
-    dbg!("{:?}", ip_kind);
+    let m = Message::Write(String::from("Hello there"));
+    m.call();
+
+    home.log_protocol();
+    loopback.log_protocol();
+
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    let absent_number: Option<i32> = None;
+
+    println!("{:#?}", some_number);
+    println!("{:#?}", some_char);
+    println!("{:#?}", absent_number);
 }
