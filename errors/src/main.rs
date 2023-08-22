557e4f6d5ad27faf37826c691059ae7474cf641a
Dan Prudhomme Jr <dannyprud@yahoo.com>
2023-08-21 20:34:37 -0500

    Complete section 9.3 completing chapter 9

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -2,6 +2,26 @@ use std::fs::{self, File};
 use std::io::{self, Read};
 use std::io::ErrorKind;
 
+#[derive(Debug)]
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
+
 fn main() {
     let greeting_file_result = File::open("src/hello.txt");  //?returns the file contents in the match chain
 
@@ -84,6 +104,13 @@ fn main() {
     };
 
     println!("{:?}", last_letter);
+
+    let good_guess = Guess::new(35);
+
+    println!("{:?}", good_guess);
+
+    let bad_guess = Guess::new(-1);
+    println!("{:?}", bad_guess);
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
