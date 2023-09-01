49dc943ccd52f2bf03b34c2222affa9256c430be
Dan Prudhomme <dannyprud@yahoo.com>
2023-09-01 15:13:46 -0500

    Save progress section 12.3

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,17 +1,42 @@
 use std::env;
 use std::fs;
 
+#[derive(Debug)]
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(query: String, file_path: String) -> Config {
+        Config { 
+            query,
+            file_path,
+        }
+    }
+
+    fn extract_file_text(self) -> String {
+        fs::read_to_string(self.file_path).expect("why no poem?")
+    }
+}
+
+
 fn main() {
     let args: Vec<String> = env::args().collect(); 
     println!("Hello, world!");
     dbg!(&args);
 
-    let query = &args[1];
-    let file_path = &args[2];
+    if args.len() < 3 {
+        panic!("Error: Please provide arguments for -- [search criteria] [file name]");
+    }
 
-    println!("Searching for {query} in {file_path}");
+    let config = parse_config(&args);
 
-    let contents = fs::read_to_string(file_path).expect("Should have been a file to read!");
+    
+    println!("{:?}", config);
+    println!("{}", config.extract_file_text());
+}
 
-    println!("With text:\n{contents}");
+fn parse_config(args: &[String]) -> Config {
+    Config::new(args[1].clone(), args[2].clone())
 }
