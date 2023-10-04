c6945f02d1c577e7b0b30a85f826b96b20e5422a
Dan Prudhomme <dannyprud@yahoo.com>
2023-10-04 10:44:22 -0500

    Refactor, better error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -18,25 +18,40 @@ impl Config {
     fn extract_file_text(self) -> String {
         fs::read_to_string(self.file_path).expect("why no poem?")
     }
+
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let config = Config::new(args[1].clone(), args[2].clone());
+
+        Ok(config)
+    }
 }
 
 
 fn main() {
     let args: Vec<String> = env::args().collect(); 
-    println!("Hello, world!");
     dbg!(&args);
 
-    if args.len() < 3 {
-        panic!("Error: Please provide arguments for -- [search criteria] [file name]");
-    }
+    // if args.len() < 3 {
+    //     panic!("Error: Please provide arguments for -- [search criteria] [file name]");
+    // }
 
-    let config = parse_config(&args);
-
+    // let config = parse_config(&args);
     
-    println!("{:?}", config);
-    println!("{}", config.extract_file_text());
+    
+    
+    let config = Config::build(&args);
+        
+    match config {
+        Ok(d) => println!("{:?}", d.extract_file_text()),
+        Err(e) => println!("{}", e),
+    }    
+    // println!("{:?}", config);
+    // println!("{}", config.extract_file_text());
 }
 
-fn parse_config(args: &[String]) -> Config {
-    Config::new(args[1].clone(), args[2].clone())
-}
+// fn parse_config(args: &[String]) -> Config {
+//     Config::new(args[1].clone(), args[2].clone())
+// }
